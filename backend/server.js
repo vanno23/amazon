8b0e2c2df545e5869bad1f5b70f0342564b11ddb
vanno23 <118561252+vanno23@users.noreply.github.com>
2023-09-20 20:29:22 +0400

    main

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,7 @@ mongoose
   });
 
 const app = express();
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -46,9 +42,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
