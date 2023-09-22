633f50f55a42380a71ad853d1c8baae1bcb03590
vanno23 <118561252+vanno23@users.noreply.github.com>
2023-09-22 14:17:01 +0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -30,15 +30,18 @@ app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
+
+const __dirname = path.resolve();
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"))
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
